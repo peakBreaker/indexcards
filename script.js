4c3d4ce12ef46df2e861a7ee310d222a4af8759e
Anders L. Hurum <andershurum@gmail.com>
2020-10-02 12:17:21 +0200

    enh: added ability to select sources

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
+const SOURCES = [
+  {"url": "https://pages.peakbreaker.com/IndexCards/dp-100.json", title: "Microsoft DP100 Exam"},
+  {"url": "https://jsonplaceholder.typicode.com/todos", title: "just a test"}
+]
+
+
 // Get the buttons and text
+var selectCourse = document.getElementById("selectCourseDropdown");
+var selectCourseSubmit = document.getElementById("selectCourseSubmit");
 var remainingCards = document.getElementById("remainingCards");
 var cardText = document.getElementById("cardText");
 var answerText = document.getElementById("answerText");
 var nextCardButton = document.getElementById("next_card");
 var switchSideButton = document.getElementById("switch_side");
 
-// Get the data
+// Variables
 var cardsData = "";
 var card = "";
-
 
 // Functions
 function getNextCard () {
@@ -35,12 +42,21 @@ function switchSide () {
   answerText.innerHTML = card.title;
 }
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-    .then(response => response.json())
-    .then(json => {cardsData = json})
-    .then(reslt => {remainingCards.innerHTML = cardsData.length})
-    .then(reslt => {getNextCard()})
+function getCards(url) {
+  fetch(url)
+      .then(response => response.json())
+      .then(json => {cardsData = json})
+      .then(reslt => {remainingCards.innerHTML = cardsData.length})
+      .then(reslt => {getNextCard()})
+}
+
+SOURCES.forEach(element => {selectCourse.innerHTML+="<option>"+element.title+"</option>"})
+console.log(selectCourse.value)
 
 // Listeneres
 nextCardButton.addEventListener("click", getNextCard);
 switchSideButton.addEventListener("click", switchSide);
+selectCourseSubmit.addEventListener("click", function(){ 
+  getCards(SOURCES.find( ({ title }) => title === selectCourse.value).url) 
+});
+
